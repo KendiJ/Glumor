22ca0bdaff6b296e5fa4b478f498c3e2aae2c935
Jacqueline Kendi <kendyjaky@gmail.com>
2021-02-03 02:15:17 +0300

    Sign in With Google set

--- lib/screens/auth/sign_in.dart
+++ lib/screens/auth/sign_in.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:glumor/app/app_colors.dart';
@@ -9,6 +11,7 @@ import 'package:glumor/screens/auth/sign_up.dart';
 import 'package:glumor/screens/pages/home.dart';
 import 'package:glumor/screens/widgets/buttons.dart';
 import 'package:glumor/screens/widgets/spacer.dart';
+import 'package:glumor/services/auth.dart';
 
 class SignIn extends StatefulWidget {
   static const id = 'sign_in';
@@ -90,6 +93,14 @@ class _SignInState extends State<SignIn> {
               ),
               onPressed: () => Navigator.pushNamed(context, Home.id),
             ),
+            Height10(),
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _signInButton(),
+              ],
+            ),
             Height40(),
             RichText(
               textAlign: TextAlign.center,
@@ -120,4 +131,42 @@ class _SignInState extends State<SignIn> {
       ),
     );
   }
+}
+
+_signInButton() {
+  return OutlineButton(
+      splashColor: Colors.grey,
+      
+      onPressed: () {
+  signInWithGoogle().then((result) {
+    if (result != null) {
+      Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => SignUp()));
+    }
+  });
+},
+  
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("assets/logo/google_logo.png"), height: 25.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
 }
